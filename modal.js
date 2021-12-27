b663d656f33dc609c5eb4052d7efb867e964b4c3
Rhyath <rhyath@gmail.com>
2021-12-27 22:43:28 +0100

    #2.1 Issue resolved - labels linked to inputs

--- modal.js
+++ modal.js
@@ -10,7 +10,11 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close");
+const formData = document.querySelectorAll(".formData"); // why is this being used? it's the label
+const inputData = document.getElementsByClassName("text-control");
+const cityRadioBtn = document.getElementsByName("location");
+const submitBtn = document.querySelectorAll(".btn-submit");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +23,33 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal event
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+/*
+// getting user data 
+const firstName = inputData[0].value;
+const lastName = inputData[1].value;
+const email = inputData[2].value;
+const birthDate = inputData[3].value;
+const noOfTournaments = inputData[4].value;
+
+for(i=0; i<cityRadioBtn.length; i++) {
+  cityRadioBtn[i].addEventListener('change', ($event) => {
+    const preferedCity = $event.target.value;
+  });
+}
+
+// keep form data after validating
+submitBtn.addEventListener('click', ($event) => {
+  $event.preventDefault();
+})
+*/
 
 
